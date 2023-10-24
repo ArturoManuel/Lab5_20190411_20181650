f87e34ca19a0aa65060b86b6ac71952adcd96589
ArturoManuel <noriegaarturonoriega@gmail.com>
2023-10-24 14:41:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab5_20190411_20181650/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab5_20190411_20181650;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lab5_20190411_20181650"
+include(":app")
+ 
